f6cc6e0ae06e993c54b09465ce52f3608b89ee60
Ted Ahlberg <ted.ahlberg95@gmail.com>
2021-07-17 19:31:14 +0200

    basic arrow and animation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header/>
+    <Arrow/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Arrow from './components/Arrow.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Arrow
   }
 }
 </script>
---
+++ src/components/Arrow.vue
@@ -0,0 +1,39 @@
+<template>
+    <button class="down">▲</button>
+</template>
+
+<script>
+</script>
+
+<style lang="css" scoped>
+button {
+    width: 35%;
+    height: 150px;
+
+    text-shadow: 0 0 20px var(--main-shadow-color);
+    font-size: 4em;
+    color: var(--main-arrow-color);
+    cursor: pointer;
+
+    padding: 0px;
+    margin: 30px auto;
+    background-color: transparent;
+    border: none;
+    display: block;
+
+    animation: breath 3s ease-out infinite;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+}
+
+.down {
+    transform: rotate(180deg);
+}
+
+@keyframes breath {
+    50% {
+        transform: scale(1.2) rotate(180deg);
+        -webkit-transform: scale(1.2) rotate(180deg);
+    }
+}
+</style>
