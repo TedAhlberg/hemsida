3582345aa8505d221a56362631211bcd13169ba1
Ted Ahlberg <ted.ahlberg95@gmail.com>
2021-07-28 19:44:43 +0200

    template fix

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,29 +1,52 @@
 <template>
     <div id="content" :class="compColor">
         <Title :contentTitle="contentTitle" />
+        <Projects v-if="projects" />
+        <Resume v-if="resume" />
+        <Contact v-if="contact" />
     </div>
 </template>
 
 <script>
     import Title from './content/Title.vue'
+    import Projects from './content/Projects.vue'
+    import Resume from './content/Resume.vue'
+    import Contact from './content/Contact.vue'
 
     export default {
         name: 'Content',
         components: {
-            Title
+            Title,
+            Projects,
+            Resume,
+            Contact
         },
         props: ['contentTitle', 'color'],
+        data: () => {
+            return ({
+                projects: false,
+                resume: false,
+                contact: false,
+            })
+        },
         computed: {
             compColor() {
-               return {['bg-color' + this.color]: true}
+                return {
+                    ['bg-color' + this.color]: true
+                }
             }
+        },
+        created() {
+            if (this.contentTitle == "PROJECTS") this.projects = true
+            if (this.contentTitle == "RESUME") this.resume = true
+            if (this.contentTitle == "CONTACT") this.contact = true
         }
     }
 </script>
 
 <style lang="css" scoped>
     #content {
-        height: 20vh;
+        height: 50vh;
         width: 100%;
         margin-bottom: -40px;
 
